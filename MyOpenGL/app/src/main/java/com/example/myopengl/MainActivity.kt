63364ff34b82955f639ea20a8f4cd7267cd82e92
chaeyoon <yoonchae125@naver.com>
2020-05-25 19:24:33 +0900

    triangle

--- MyOpenGL/app/src/main/java/com/example/myopengl/MainActivity.kt
+++ MyOpenGL/app/src/main/java/com/example/myopengl/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.myopengl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var btn_triangle:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_triangle = findViewById(R.id.btn_triangle)
+
+        var triangleIntent = Intent(this,HelloTriangleActivity::class.java)
+        btn_triangle.setOnClickListener { startActivity(triangleIntent) }
     }
 }
